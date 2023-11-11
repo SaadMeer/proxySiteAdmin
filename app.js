ccd10fb3a9ae1b6706144230d19516cb999cad37
SaadMeer <saad.wicks@gmail.com>
2023-11-11 13:49:19 +0500

    Admin Sidebar fix

--- app.js
+++ app.js
@@ -18,53 +18,113 @@ $(".progress-1").each(function(){
   });
 });
 
-
-
 const toggleBox = document.querySelector('.deskyop-arrow');
 
 
 toggleBox.addEventListener('click', () => {
-    const sidebarMain = document.querySelector('.sidebar-main-container');
-    const sidebarSpan = document.querySelector('.sidebar-ul');
-    const logoname = document.querySelector('.logoname');
+  const sidebarMain = document.querySelector('.sidebar-main-container');
+  const sidebarSpan = document.querySelector('.sidebar-ul');
+  const logoname = document.querySelector('.logoname');
+  const logBtn = document.querySelector('.logout-btn');
 
-    const bottomUserName = document.querySelector('.bottom-user-name');
-    // const logoname = document.querySelector('.logoname');
+  const dash1 = document.querySelector('.dash-li-1');
+  const dash2 = document.querySelector('.dash-li-2');
+  const dash3 = document.querySelector('.dash-li-3');
+  const dash4 = document.querySelector('.dash-li-4');
 
-
-    const leftarrowMenu = document.querySelectorAll('.leftarrow-menu');
-    const sidebarli = document.querySelectorAll('.sidebar-li');
-    const spans = sidebarSpan.getElementsByTagName("span");
-
-
-    document.querySelector('.right-content-section').classList.toggle('right-content-section-flex')
-    sidebarMain.classList.toggle("hide-sidebar");
-    toggleBox.classList.toggle("toggle-box-container-show");
-    logoname.classList.toggle("logonamehide");
-    document.querySelector('.main-logo').style.marginLeft = '17px';
-    bottomUserName.classList.toggle("bottom-user-name-hide");
+  const bottomUserName = document.querySelector('.bottom-user-name');
+  // const logoname = document.querySelector('.logoname');
 
 
-    for (let i = 0; i < spans.length; i++) {
-        spans[i].classList.toggle("dislpay-none");
+  const leftarrowMenu = document.querySelectorAll('.leftarrow-menu');
+  const sidebarli = document.querySelectorAll('.sidebar-li');
+  const spans = sidebarSpan.getElementsByTagName("span");
 
-    }
-    for (let i = 0; i < sidebarli.length; i++) {
-        sidebarli[i].classList.toggle("sidebar-icon-center");
-    }
-    for (let i = 0; i < sidebarli.length; i++) {
-        leftarrowMenu[i].classList.toggle("leftarrow-menu-hide");
+  
+  document.querySelector('.right-content-section').classList.toggle('right-content-section-flex')
+  sidebarMain.classList.toggle("hide-sidebar");
+  toggleBox.classList.toggle("toggle-box-container-show");
+  logoname.classList.toggle("logonamehide");
+  document.querySelector('.main-logo').style.marginLeft = '17px';
+  logBtn.classList.toggle('logout-btn-none');
+  bottomUserName.classList.toggle("bottom-user-name-hide");
 
-    }
+  dash1.classList.toggle("dash-li-name-show-1");
+  dash2.classList.toggle("dash-li-name-show-2");
+  dash3.classList.toggle("dash-li-name-show-3");
+  dash4.classList.toggle("dash-li-name-show-4");
+  
+
+  for (let i = 0; i < spans.length; i++) {
+    spans[i].classList.toggle("dislpay-none");
+
+  }
+  for (let i = 0; i < sidebarli.length; i++) {
+    sidebarli[i].classList.toggle("sidebar-icon-center");
+  }
+  for (let i = 0; i < sidebarli.length; i++) {
+    leftarrowMenu[i].classList.toggle("leftarrow-menu-hide");
+  }
 
 })
 
 
 function toggleSidebar() {
-    var sidebar = document.querySelector('.sidebar-main-container');
-    if (sidebar.style.display === 'none') {
-      sidebar.style.display = 'block';
-    } else {
-      sidebar.style.display = 'none';
-    }
+  var sidebar = document.querySelector('.sidebar-main-container');
+  if (sidebar.style.display === 'none') {
+    sidebar.style.display = 'block';
+  } else {
+    sidebar.style.display = 'none';
   }
+}
+
+
+// const toggleBox = document.querySelector('.deskyop-arrow');
+
+
+// toggleBox.addEventListener('click', () => {
+//     const sidebarMain = document.querySelector('.sidebar-main-container');
+//     const sidebarSpan = document.querySelector('.sidebar-ul');
+//     const logoname = document.querySelector('.logoname');
+
+//     const bottomUserName = document.querySelector('.bottom-user-name');
+//     // const logoname = document.querySelector('.logoname');
+
+
+//     const leftarrowMenu = document.querySelectorAll('.leftarrow-menu');
+//     const sidebarli = document.querySelectorAll('.sidebar-li');
+//     const spans = sidebarSpan.getElementsByTagName("span");
+
+
+//     document.querySelector('.right-content-section').classList.toggle('right-content-section-flex')
+//     sidebarMain.classList.toggle("hide-sidebar");
+//     toggleBox.classList.toggle("toggle-box-container-show");
+//     logoname.classList.toggle("logonamehide");
+//     document.querySelector('.main-logo').style.marginLeft = '17px';
+//     bottomUserName.classList.toggle("bottom-user-name-hide");
+
+
+//     for (let i = 0; i < spans.length; i++) {
+//         spans[i].classList.toggle("dislpay-none");
+
+//     }
+//     for (let i = 0; i < sidebarli.length; i++) {
+//         sidebarli[i].classList.toggle("sidebar-icon-center");
+//     }
+//     for (let i = 0; i < sidebarli.length; i++) {
+//         leftarrowMenu[i].classList.toggle("leftarrow-menu-hide");
+
+//     }
+
+// })
+
+
+// function toggleSidebar() {
+//     var sidebar = document.querySelector('.sidebar-main-container');
+//     if (sidebar.style.display === 'none') {
+//       sidebar.style.display = 'block';
+//     } else {
+//       sidebar.style.display = 'none';
+//     }
+//   }
+
